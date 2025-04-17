53d54fff259bff693e564ee5a72a4df4595b3575
Ryan0042-git <ryanmarleylimafontes@gmail.com>
2025-04-17 15:26:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Função do botão de Login
+
 function acionarBotaoLogin() {
     var textoNome = document.getElementById('txtNome').value;
     var textoEndereço = document.getElementById('txtEndereco').value;
@@ -8,29 +8,25 @@ function acionarBotaoLogin() {
     } else if(textoEndereço == "") {
       alert("Digite seu endereço!!!");
     } else {
-      if(textoEmail == "senac@gmail.com" && textoSenha == "senac2025") {
-        window.location.href = 'index.html'; // Redireciona para a página de teste
+      if(textoNome == "Ryan" && textoEndereço == "rua fajao 97") {
+        window.location.href = 'index.html';
       } else {
         alert("Nome ou endereço incorreto!!");
       }
     }
   }
   
-  // Função do botão de Cadastro
   function acionarBotaoCadastro() {
     var textoNome = document.getElementById('txtNome').value;
     var textoEmail = document.getElementById('txtEmail').value;
-    var textoMensagem = document.getElementById('txtMensagem').value;
-  
+
     if(textoNome == "") {
       alert("Preencha o campo nome!!!");
     } else if(textoEmail == "") {
       alert("Preencha o campo email");
-    } else if(textoMensagem == "") {
-      alert("Digite a mensagem");
     } else {
-      if(textoMensagem == "pizza" && textoNome == "Ryan") {
-        window.location.href = 'login.html'; // Redireciona para a página de teste
+      if(textoNome == "Ryan") {
+        window.location.href = 'login.html'; 
       } else {
         alert("Infelizmente não foi possivel efetuar o cadastro");
         return;
@@ -38,3 +34,44 @@ function acionarBotaoLogin() {
     }
   }
   
+  async function rastrearPedido() {
+    const numero = document.getElementById('numeroPedido').value;
+    const resultado = document.getElementById('resultadoRastreamento');
+
+    if (!numero) {
+      alert("Digite o número do pedido!");
+      return;
+    }
+
+    resultado.innerText = "Consultando pedido...";
+
+    async function rastrearPedido() {
+      const numero = document.getElementById('numeroPedido').value;
+      const resultado = document.getElementById('resultadoRastreamento');
+    
+      if (!numero) {
+        alert("Digite o número do pedido!");
+        return;
+      }
+    
+      resultado.innerText = "Consultando pedido...";
+    
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    
+      const pedidosFake = {
+        "123": { status: "Pedido em separação" },
+        "456": { status: "Pedido em transporte" },
+        "789": { status: "Entregue" },
+        "321": { status: "Cancelado" }
+      };
+    
+      const dados = pedidosFake[numero];
+    
+      if (dados && dados.status) {
+        resultado.innerText = `Status do Pedido #${numero}: ${dados.status}`;
+      } else {
+        resultado.innerText = "Pedido não encontrado.";
+      }
+    
+    }
+  }
